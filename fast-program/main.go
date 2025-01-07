c2d6c4655beb31a3eb5051c0192cce4cd6dfcc61
MD.Kamrul Hasan ⚡️ <kamrulhasan635653@gmail.com>
2025-01-07 23:34:45 +0600

    veriable init

--- fast-program/main.go
+++ fast-program/main.go
@@ -3,5 +3,28 @@ package main
 import "fmt"
 
 func main(){
-fmt.Print("Hello world!");
+	// fmt.Print("Hello world!");
+
+	/*
+		int
+		float32
+		bool
+		string
+	*/
+
+
+
+	// a := 10
+	a := true
+
+	a = false
+
+	const p = 100
+	fmt.Println(p)
+
+	fmt.Println(a)
+
+	a = true
+
+	fmt.Println(a)
 }
